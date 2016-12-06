525326d356304f23c331d1ac096d6f9956e6ed4d
mal-wood <mallorywood413@gmail.com>
2016-12-06 13:02:21 -0800

    Set up routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
 
   def index
   end 
-  
+
+  def about
+  end 
+
+  def connect
+  end 
+
+  def work
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root "pages#index"
+  get 'home' => "pages#index"
+  get 'about' => "pages#about"
+  get 'connect' => "pages#connect"
+  get 'work' => "pages#work"
 end
